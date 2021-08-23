cab2244a5138b0a9b53b7deb879fbea0e64a7ecb
Khalildkk <71195218+Khalildkk@users.noreply.github.com>
2021-08-23 11:52:12 +0100

    Update bearer.js

--- src/drivers/auth/bearer.js
+++ src/drivers/auth/bearer.js
@@ -1,19 +1,16 @@
 export default {
 
     request: function (req, token) {
-        console.log("bearer: " + token)
         this.drivers.http.setHeaders.call(this, req, {
-            Authorization: 'Bearer ' + token
+            Authorization: 'Bearer ' + tokn
         });
     },
 
     response: function (res) {
-        console.log("bearer: " + JSON.stringify(header))
         var headers = this.drivers.http.getHeaders.call(this, res),
             token   = headers.Authorization || headers.authorization;
             
         if (token) {
-            console.log("token: " + token)
             token = token.split(/Bearer:?\s?/i);
 
             return token[token.length > 1 ? 1 : 0].trim();
